58a9debaad91b6323d5bcf8ce1e6702d4383c4b3
Jack <jhughes1153@aol.com>
2019-06-13 19:50:05 -0400

    merged master file into branch

--- chatbot_models.py
+++ chatbot_models.py
@@ -7,7 +7,6 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
 GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
@@ -94,15 +93,15 @@ def test_model(chat_bot_model):
         else:
             print("ROBO: Bye! take care...")
             break
-            
-            
+
+
 def create_model_readable(path, json_path):
     with open(json_path, 'r') as f:
         json_temp = json.load(f)
 
     df = pd.DataFrame(json_temp)
-    
-    with open('/home/jack/groupme.txt', 'w') as f:
+
+    with open(path, 'w') as f:
         for counter, row in df.iterrows():
             if row['text'] is None:
                 print(f"Not adding {counter}")
@@ -116,7 +115,7 @@ def main():
     parser.add_argument('-j', '--json-path', help="accepts a json file as input and creates a new txt file and keeps"
                                                   "that new file in the model, set the location to the path arguments")
     args = parser.parse_args()
-    
+
     if args.json_path is not None:
         create_model_readable(args.path, args.json_path)
 
