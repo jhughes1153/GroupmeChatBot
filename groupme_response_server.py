306da15d279e64d22a1a46a0c6a99b51464f37a8
jackh <jhughes1153@aol.com>
2019-06-21 19:00:12 -0400

    Start of using asyncio module for python3 requests to send to and fro the groupme messages

--- groupme_response_server.py
+++ groupme_response_server.py
@@ -1,39 +1,60 @@
-from flask import Flask, request, jsonify
+import requests
 from argparse import ArgumentParser
 import chatbot_models
+import asyncio
 
 """
-Should this be in the main function????
+Main server, makes requests and send messages to chatbot
 
-Should this just be a flask app?????
+Lets worry about database connectivity after we already have this up and running
+as it currently takes a static file, but ChatBotModel can take a file at any
+time so we can add that later
 """
 
-app = Flask(__name__)
+
+class MessageHandler:
+    def __init__(self, messages):
+        self.messages = messages
+
+    def reset_messages(self):
+        self.messages = []
 
 
-@app.route('/get_response', methods=['POST'])
-def send_message():
-    """
-    Expects a json in the form of {"Message": "Sentence goes here"}
-    :return:
-    """
-    context = request.json
-    user_message = context['Message']
-    print(context['Message'])
-
-    return jsonify({"ChatBotMessage": chatbot_models.respond(chat_bot, user_message)})
+async def read_message(response_url, request_params):
+    while True:
+        await asyncio.sleep(60)
+        messages = requests.get(response_url, params=request_params)
 
 
-def setup():
-    parser = ArgumentParser(description="run a server to check a file and write to a new one if there is a new message"
-                                        "in the file waiting to get a response")
-    parser.add_argument('-i', '--input-path', help='file to check for a new message')
+async def send_message():
+    while True:
+        await asyncio.sleep(5)
+
+
+def main():
+    parser = ArgumentParser(description='Runs the server for the groupmechatbot')
+    parser.add_argument('-t', '--token', help='Groupme token', required=True)
+    parser.add_argument('-a', '--account', help='Database account to access')
+    parser.add_argument('-i', '--model-input-file', help='txt file for the model')
     args = parser.parse_args()
 
-    return chatbot_models.ChatBotModel(args.input_path)
+    request_params = {'token': args.token}
+    response_url = 'https://api.groupme.com/v3/groups/16915455/messages'
 
+    chatbot = chatbot_models.ChatBotModel(args.model_input_file)
+
+    loop = asyncio.get_event_loop()
+
+    try:
+        asyncio.ensure_future(read_message(request_params, response_url))
+        asyncio.ensure_future(send_message())
+
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('Closing loop')
+        loop.close()
 
 if __name__ == '__main__':
-    chat_bot = setup()
-
-    app.run(host='0.0.0.0', debug=True)
+    main()
